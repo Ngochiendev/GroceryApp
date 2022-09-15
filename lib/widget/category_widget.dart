361937ec5048ee19203dc3805dac10b29e1f7930
Võ Ngọc Hiện <macos@MacBook-Pro-cua-Vo.local>
2022-09-15 21:45:10 +0700

    UI Category Screen

---
+++ lib/widget/category_widget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:groceryapp/provider/dark_theme_provider.dart';
+import 'package:groceryapp/widget/text_widget.dart';
+import 'package:provider/provider.dart';
+
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget({
+    super.key,
+    required this.catText,
+    required this.imgPath,
+    required this.passedColor,
+  });
+
+  final String catText, imgPath;
+  final Color passedColor;
+  @override
+  Widget build(BuildContext context) {
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    double screenWidth = MediaQuery.of(context).size.width;
+    final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
+    return InkWell(
+      onTap: () {
+        print('Category pressed');
+      },
+      child: Container(
+        // height: _screenWidth * 0.6,
+        decoration: BoxDecoration(
+          color: passedColor.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: passedColor.withOpacity(0.7),
+            width: 2,
+          ),
+        ),
+        child: Column(children: [
+          // Container for the image
+          Container(
+            height: screenWidth * 0.3,
+            width: screenWidth * 0.3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    imgPath,
+                  ),
+                  fit: BoxFit.fill),
+            ),
+          ),
+          // Category name
+          TextWidget(
+            text: catText,
+            color: color,
+            textSize: 20,
+            isTitle: true,
+          ),
+        ]),
+      ),
+    );
+  }
+}
