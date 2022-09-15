5302cefb154a66dfaa947d6ad22181138cbf0394
Võ Ngọc Hiện <macos@MacBook-Pro-cua-Vo.local>
2022-09-15 21:46:16 +0700

    UI Home Screen

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:groceryapp/ultils/ultils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +10,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<String> _listImage = [
+    './assets/images/offres/Offer1.jpg',
+    './assets/images/offres/Offer2.jpg',
+    './assets/images/offres/Offer3.jpg',
+    './assets/images/offres/Offer4.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    final themeState = Utils(context).getTheme;
+    final size = Utils(context).size;
+    return Scaffold(
+      body: SizedBox(
+        height: size.height * 0.33,
+        width: size.width,
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return Image.asset(
+              _listImage[index],
+              fit: BoxFit.fill,
+            );
+          },
+          autoplay: true,
+          itemCount: _listImage.length,
+          pagination: const SwiperPagination(
+            alignment: Alignment.bottomCenter,
+            builder: DotSwiperPaginationBuilder(
+              color: Colors.white,
+              activeColor: Colors.blueAccent,
+            ),
+          ),
+          control: const SwiperControl(),
+        ),
       ),
     );
   }
